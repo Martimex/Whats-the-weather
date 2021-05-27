76cb1933a6d8bbfba115949e583c1e15e2f96fee
Martimex <kamilmarciniak533@wp.pl>
2021-05-27 14:34:16 +0200

    Attach detail page to the app
    App now contains detail app, but for now it needs to be configured so
    that it looks cool also on mobile devices. The second major issue is
    of course setup a connection from page to page, sending city name to
    detail page in order to render correctly.

--- controllers/all_Pages.js
+++ controllers/all_Pages.js
@@ -7,6 +7,6 @@ exports.app_page = async (req, res, next) => {
 };
 
 exports.detail_page = async (req, res, next) => {
-    await res.send(`Not implemented: Detail page`);
-    //await res.render('detail_page', {title: `What's The Weather`, query: query})
+    //await res.send(`Not implemented: Detail page`);
+    await res.render('detail_page', {title: `What's The Weather`})
 };
