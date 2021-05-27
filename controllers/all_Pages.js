89ac39b40733e7ef3a4c7069c5e53ac8551df4f7
Martimex <kamilmarciniak533@wp.pl>
2021-05-27 10:00:51 +0200

    Add comment in all_Pages.js

--- controllers/all_Pages.js
+++ controllers/all_Pages.js
@@ -8,4 +8,5 @@ exports.app_page = async (req, res, next) => {
 
 exports.detail_page = async (req, res, next) => {
     await res.send(`Not implemented: Detail page`);
+    //await res.render('detail_page', {title: `What's The Weather`, query: query})
 };
