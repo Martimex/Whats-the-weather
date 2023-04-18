16e7649cd9eb4616db82533c201ca2b44e7fc1b6
Martimex <kamilmarciniak533@wp.pl>
2023-04-18 21:13:23 +0200

    v11

--- controllers/all_Pages.js
+++ controllers/all_Pages.js
@@ -1,10 +1,10 @@
-exports.index = async (req, res, next) => {
+/* exports.index = async (req, res, next) => {
    await res.render('landing_page', {title: `What's The Weather`});
 };
-
-exports.app_page = async (req, res, next) => {
+ */
+exports.index = async (req, res, next) => {
    //await res.header(`Content-Security-Policy: connect-src http://api.openweathermap.org/data/2.5/; style-src https://fonts.googleapis.com;`);
-   await res.render('app_page', {title:  `What's The Weather`});
+   await res.render('index', {title:  `What's The Weather`});
 };
 
 exports.detail_page = async (req, res, next) => {
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 const all_Pages = require('../controllers/all_Pages');
 
-router.get('/cities', all_Pages.app_page);
+router.get('/cities', all_Pages.index);
 
 router.get('/cities/:cityname,:countrycode&units=:unit', all_Pages.detail_page);
 
