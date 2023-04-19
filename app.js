546c0d6ce421a8fef1220258250d2cf0522c0f4f
Martimex <kamilmarciniak533@wp.pl>
2023-04-19 09:05:42 +0200

    v13

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ const helmet = require(`helmet`);
 var app = express();
 
 // view engine setup
+app.engine('pug', require('pug').__express);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
